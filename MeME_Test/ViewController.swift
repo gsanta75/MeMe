4f593aeb8bde67e6bd1d3bdce513b746a31f765c
Giuseppe Santaniello <giuseppesantaniello@yahoo.it>
2015-06-30 00:06:42 +0200

    Initial Commit

---
+++ MeME_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeME_Test
+//
+//  Created by GIUSEPPE SANTANIELLO on 30/06/15.
+//  Copyright (c) 2015 GIUSEPPE SANTANIELLO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
